b99b2b5ccc1c43b47b427af67ab2d7c57dba77c4
Madison Dellamea <mdellamea@lcs.on.ca>
2022-02-22 10:20:05 -0600

    Initial Commit

---
+++ DadJokes/DadJokesApp.swift
@@ -0,0 +1,17 @@
+//
+//  DadJokesApp.swift
+//  DadJokes
+//
+//  Created by Madison Dellamea on 2/22/22.
+//
+
+import SwiftUI
+
+@main
+struct DadJokesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
