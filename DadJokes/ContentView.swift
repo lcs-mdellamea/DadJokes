b056a5e286a07c6118d5046da20ef7959687368a
Madison Dellamea <mdellamea@lcs.on.ca>
2022-02-22 10:41:16 -0600

    Laid out a static interface for the app.

--- DadJokes/ContentView.swift
+++ DadJokes/ContentView.swift
@@ -2,20 +2,68 @@
 //  ContentView.swift
 //  DadJokes
 //
-//  Created by Madison Dellamea on 2/22/22.
+//  Created by Russell Gordon on 2022-02-21.
 //
 
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            
+            Text("How do you organize a space party? You planet.")
+                .font(.largeTitle)
+                .multilineTextAlignment(.leading)
+                .padding(30)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 16)
+                        .stroke(Color.primary, lineWidth: 4)
+                )
+            
+            Image(systemName: "heart.circle")
+                .resizable()
+                .frame(width: 30, height: 30)
+                .foregroundColor(.gray)
+            
+            Button(action: {
+                
+                print("I've been pressed!")
+                
+            }, label: {
+                Text("Another one!")
+                    //.font(.largeTitle)
+            })
+                .padding()
+                .buttonStyle(.bordered)
+            
+            
+            HStack {
+                
+                Text("Favourites")
+                    .bold()
+                
+                Spacer()
+            }
+            
+            List {
+                Text("Which side of the chicken has more feathers? The outside.")
+                Text("Why did the Clydesdale give the pony a glass of water? Because he was a little horse!")
+                Text("The great thing about stationery shops is they're always in the same place...")
+            }
+            
+            Spacer()
+                        
+        }
+        .navigationTitle("icanhazdadjoke?")
+        .padding()
     }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        NavigationView {
+            ContentView()
+        }
     }
 }
